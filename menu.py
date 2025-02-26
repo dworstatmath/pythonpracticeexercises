a827f93145379ce750e847eaad9cd59439e9e3ff
dworstatmath <ian.alexander.ng@gmail.com>
2025-02-26 18:51:07 +0000

    menu

---
+++ menu.py
@@ -0,0 +1,6 @@
+menu = { "Pizza": 1.99, "Soda":  0.69, "Double Chunk Chocolate Chip Cookie": 2.49}
+
+def additem(a,b):
+    menu[a]=b
+additem("Giant 200-layer, 3-flavor Wedding Cake w/ Edible Gold Leaf",0.09)
+print(menu)
